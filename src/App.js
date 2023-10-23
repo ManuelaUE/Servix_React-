7de1008bb423b083bab657b50bcdb7024406e134
Santiago Leon <93327269+Leonslp@users.noreply.github.com>
2023-10-23 18:56:48 -0500

    Integracion de login
    Se integra el login con las demas paginas ya integradas

--- src/App.js
+++ src/App.js
@@ -3,16 +3,14 @@ import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 
 import Login from './containers/Login';
 import Register from './containers/Register';
-
 import Home from './containers/Home';
 
 function App () {
     return (
         <Router>
             <Routes>
-                <Route path="/" element={<Login />} />
+                <Route path="/login" element={<Login />} />
                 <Route path="/register" element={<Register />} />
-
                 <Route path="/home" element={<Home />} />
             </Routes>
         </Router>
