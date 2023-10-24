267c33c81e0d48de48dfa0f87f5ff9cd5da3818a
Santiago Leon <93327269+Leonslp@users.noreply.github.com>
2023-10-23 22:08:45 -0500

    Cambios en .jason

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 
 import Login from './containers/Login';
 import Register from './containers/Register';
+
 import Home from './containers/Home';
 
 function App () {
     return (
         <Router>
             <Routes>
+
                 <Route path="/login" element={<Login />} />
                 <Route path="/register" element={<Register />} />
+
                 <Route path="/home" element={<Home />} />
             </Routes>
         </Router>
